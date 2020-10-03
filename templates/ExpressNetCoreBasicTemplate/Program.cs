b639197f6162f7d80194b61803544ea3141003a4
Ronny Gunawan <3048897+ronnygunawan@users.noreply.github.com>
2020-10-03 23:54:08 +0700

    Added basic project template

---
+++ templates/ExpressNetCoreBasicTemplate/Program.cs
@@ -0,0 +1,8 @@
+﻿using ExpressNetCore;
+
+ExpressApp app = new ExpressApp(args);
+
+app.MapGet("/", context => context.Response.RenderRazorViewAsync("Home/Index"));
+app.MapGet("/about", context => context.Response.RenderRazorViewAsync("Home/About"));
+
+app.Run();
